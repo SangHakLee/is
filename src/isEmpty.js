ea6b66179ce6c9e6f1f5c120301e089c8ac9e142
sanghaklee <code.ryan.lee@gmail.com>
2019-06-21 18:24:57 +0900

    isEmpty 추가isEmpty 추가

--- src/isEmpty.js
+++ src/isEmpty.js
@@ -1,11 +1,28 @@
+const debug = require('debug')('isEmpty')
+
 /**
- * 
+ * @description Check if parameter is empty
+ * 'empty' is an empty string, an empty array, an empty object, null, undefined
  * @param {object} value 
  */
 const isEmpty = (value) => {
-	if (value === '' || value === null || value === undefined || (value !== null && typeof value === 'object' && !Object.keys(value).length)) {
-		return true
-	} 
+	debug('value', value)
+	debug('typeof value', typeof value)
+	debug('Object.keys(value).length', Object.keys(value).length)
+	debug('value.constructor.name', value.constructor.name)
+
+	if (value === null) return true
+	if (typeof value === 'undefined') return true
+	if (typeof value === 'string' && value === '') return true
+	if (Array.isArray(value) && value.length < 1) return true // [1]
+	if (typeof value === 'object' && value.constructor.name === 'Object' && Object.keys(value).length < 1) return true // {a:1}
+	// if (typeof value === 'object')
+
+	// if (value === '' || value === null || value === undefined || (value !== null && typeof value === 'object' && !Object.keys(value).length)) {
+	// 	return true
+	// } 
+
+	debug('isEmpty')
 	return false
 }
 
--- test/bmt.spec.js
+++ test/bmt.spec.js
@@ -6,36 +6,44 @@ const underscore = require('underscore')
 const lodash = require('lodash')
 
 const modules = [
-    {name: 'my', module: my},
-    // {name: 'underscore', module: underscore},
-    // {name: 'lodash', module: lodash},
+	{name: 'my', module: my},
+	// {name: 'underscore', module: underscore},
+	// {name: 'lodash', module: lodash},
 ]
 
+function Car() {
+
+}
+
 const valid = [
-    '', [], {}
+	// '', [], {}, undefined, null
 ]
 const invalid = [
-    true, 1, 1.0, 'string', NaN , [1], {a:1}
+	// true, false,
+	// Symbol, NaN, new Date(), new RegExp(), new Error(), new Function(),
+	// 1, 1.0, 'string' , [1], {a:1}
+
+	new Number()
 ]
 
 describe('isEmpty', function() {
-    modules.forEach((m) => {
-        describe(`${m.name}`, () => {
-            const module = m.module
-            valid.forEach((v) => {
-                it(`[valid] isEmpty(${JSON.stringify(v)})`, () => {
-                    const r = module.isEmpty(v)
-                    debug('r', r)
-                    r.should.be.True()
-                })
-            })
-            invalid.forEach((v) => {
-                it(`[invalid] isEmpty(${JSON.stringify(v)})`, () => {
-                    const r = module.isEmpty(v)
-                    debug('r', r)
-                    r.should.be.False()
-                })
-            })
-        })
-    })
+	modules.forEach((m) => {
+		describe(`${m.name}`, () => {
+			const module = m.module
+			valid.forEach((v) => {
+				it(`[valid] isEmpty(${JSON.stringify(v)})`, () => {
+					const r = module.isEmpty(v)
+					debug('r', r)
+					r.should.be.True()
+				})
+			})
+			invalid.forEach((v) => {
+				it(`[invalid] isEmpty(${JSON.stringify(v)})`, () => {
+					const r = module.isEmpty(v)
+					debug('r', r)
+					r.should.be.False()
+				})
+			})
+		})
+	})
 })
